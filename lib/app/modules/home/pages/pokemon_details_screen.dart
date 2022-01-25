2c4823e3ae982915c597ad7eb5c68b2d570f5a40
Matheus Timbó <matheusfild4@hotmail.com>
2022-01-25 18:27:16 -0300

    Adding animations

--- lib/app/modules/home/pages/pokemon_details_screen.dart
+++ lib/app/modules/home/pages/pokemon_details_screen.dart
@@ -17,16 +17,69 @@ class PokemonDetailsScreen extends StatefulWidget {
   _PokemonDetailsScreenState createState() => _PokemonDetailsScreenState();
 }
 
-class _PokemonDetailsScreenState extends State<PokemonDetailsScreen> {
+class _PokemonDetailsScreenState extends State<PokemonDetailsScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _cardController;
+  late AnimationController _cardHeightController;
+  late AnimationController _rotateController;
+
+  @override
+  void initState() {
+    _cardController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _cardHeightController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 220),
+    );
+
+    _rotateController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 5000),
+    );
+    _rotateController.repeat();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cardController.dispose();
+    _cardHeightController.dispose();
+    _rotateController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final infoPanelMinHeight = MediaQuery.of(context).size.height -
+        (MediaQuery.of(context).padding.top +
+            120 +
+            MediaQuery.of(context).size.width * 0.6);
+    final infoPanelMaxHeight = MediaQuery.of(context).size.height -
+        (MediaQuery.of(context).padding.top + 48);
     return Scaffold(
       backgroundColor: widget.pokemon.color,
       body: Stack(
         alignment: Alignment.topCenter,
         children: [
-          const PokemonInfoPanel(),
-          PokemonImage(widget: widget),
+          RotatingPokeball(
+            rotateController: _rotateController,
+          ),
+          PokemonInfoPanel(
+            cardController: _cardController,
+            infoPanelMinHeight: infoPanelMinHeight,
+            infoPanelMaxHeight: infoPanelMaxHeight,
+          ),
+          PokemonImage(
+            widget: widget,
+            cardController: _cardController,
+            infoPanelMinHeight: infoPanelMinHeight,
+            infoPanelMaxHeight: infoPanelMaxHeight,
+          ),
           PokemonDetailsHeader(
             widget: widget,
           ),
@@ -36,23 +89,50 @@ class _PokemonDetailsScreenState extends State<PokemonDetailsScreen> {
   }
 }
 
+class RotatingPokeball extends StatelessWidget {
+  final AnimationController rotateController;
+  const RotatingPokeball({Key? key, required this.rotateController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: -(MediaQuery.of(context).size.width * 0.08),
+      top: MediaQuery.of(context).size.width * 0.55,
+      child: RotationTransition(
+        turns: rotateController,
+        child: Image.asset(
+          'assets/images/pokeball.png',
+          width: MediaQuery.of(context).size.width / 2,
+          height: MediaQuery.of(context).size.width / 2,
+          color: Colors.white.withOpacity(0.2),
+        ),
+      ),
+    );
+  }
+}
+
 class PokemonInfoPanel extends StatelessWidget {
+  final AnimationController cardController;
+  final double infoPanelMinHeight;
+  final double infoPanelMaxHeight;
+
   const PokemonInfoPanel({
     Key? key,
+    required this.cardController,
+    required this.infoPanelMinHeight,
+    required this.infoPanelMaxHeight,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SlidingUpPanel(
-      minHeight: MediaQuery.of(context).size.height -
-          (MediaQuery.of(context).padding.top +
-              120 +
-              MediaQuery.of(context).size.width * 0.6),
-      maxHeight: MediaQuery.of(context).size.height -
-          (MediaQuery.of(context).padding.top + 48),
+      minHeight: infoPanelMinHeight,
+      maxHeight: infoPanelMaxHeight,
       panel: const Center(
         child: Text("This is the sliding Widget"),
       ),
+      onPanelSlide: (position) => cardController.value = position,
     );
   }
 }
@@ -152,34 +232,55 @@ class PokemonDetailsHeader extends StatelessWidget {
 }
 
 class PokemonImage extends StatelessWidget {
+  final PokemonDetailsScreen widget;
+  final AnimationController cardController;
+  final double infoPanelMinHeight;
+  final double infoPanelMaxHeight;
+
   const PokemonImage({
     Key? key,
     required this.widget,
+    required this.cardController,
+    required this.infoPanelMinHeight,
+    required this.infoPanelMaxHeight,
   }) : super(key: key);
-
-  final PokemonDetailsScreen widget;
 
   @override
   Widget build(BuildContext context) {
+    final translationAmount = infoPanelMaxHeight - infoPanelMinHeight;
     return Positioned(
       top: MediaQuery.of(context).padding.top + 120,
-      child: Hero(
-        tag: 'pokemon_sprite_frontdefault',
-        child: CachedNetworkImage(
-          imageUrl: widget.pokemon.imageUrl,
-          placeholder: (context, url) => const SizedBox(),
-          errorWidget: (context, url, error) => const SizedBox(),
-          imageBuilder: (context, imageProvider) => Container(
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: MediaQuery.of(context).size.width * 0.7,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
+      child: AnimatedBuilder(
+        animation: cardController,
+        builder: (context, _) {
+          return Transform.translate(
+            offset: Offset(
+              0,
+              -(translationAmount * cardController.value),
+            ),
+            child: Opacity(
+              opacity: 1 - cardController.value,
+              child: Hero(
+                tag: 'pokemon_sprite_frontdefault',
+                child: CachedNetworkImage(
+                  imageUrl: widget.pokemon.imageUrl,
+                  placeholder: (context, url) => const SizedBox(),
+                  errorWidget: (context, url, error) => const SizedBox(),
+                  imageBuilder: (context, imageProvider) => Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: MediaQuery.of(context).size.width * 0.7,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
